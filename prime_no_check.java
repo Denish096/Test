7ddec8c7764909c551fef2064a86a4d37a98a837
Denish096 <134671249+Denish096@users.noreply.github.com>
2025-03-05 17:49:55 +0530

    Update prime_no_check.java

--- prime_no_check.java
+++ prime_no_check.java
@@ -3,13 +3,13 @@ package test;
 public class prime_no_check {
 	public static void main(String[] args){ 
 		for (int i=1;i<=1000;i++) {
-			boolean b=PerfectNo(i);
+			boolean b= primeNo(i);
 			if(b==true) {
 			System.out.println(i);}
 			}
 		}
 			
-		public	static boolean PerfectNo(int n)
+		public	static boolean primeNo(int n)
 			{
 			for (int i=2;i<=(n/2);i++)
 			if(n%i==0)
